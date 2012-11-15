6ba47db0f8cc4f254368f3307815c090d8395591
Marian de Bonis <marie-antoinette.de-bonis-hamelin@hp.com>
2012-11-14 16:38:49 -0800

    ENYO-1449:  testsuite setup using kinds defined in lib/extra/test (TestReporter/TestRunner/TestSuite) ServiceRegistryTest implemented as first test case. Enyo-DCO-1.1-Signed-off-by: Marie-Antoinette de Bonis-Hamelin (Marian) marie-antoinette.de-bonis-hamelin@hp.com

---
+++ test/tests/ServiceRegistryTest.js
@@ -0,0 +1,19 @@
+enyo.kind({
+	name: "ServiceRegistryTest",
+	kind: enyo.TestSuite,
+	components: [
+              {name: "serviceRegistry", kind: "ServiceRegistry"}
+    ],    
+    service: "home",
+	testGetHomeService: function(service) {
+		var self = this;
+		this.$.serviceRegistry._reloadServices(function(err){
+			if (err) {
+				fail;
+			} else {
+				var s = self.$.serviceRegistry.resolveServiceId(self.service);
+    			self.finish(self.$.serviceRegistry.getServiceId(s) ? "" : "no '" + self.service + "' service registered");
+    		}
+		});
+    }
+});
